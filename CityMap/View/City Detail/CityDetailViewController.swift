ab331a8ef5abe467f7f9ec26d5397de3797e39f8
aaaus <aaausaaa@gmail.com>
2019-02-27 23:53:34 +0200

    8

--- CityMap/View/City Detail/CityDetailViewController.swift
+++ CityMap/View/City Detail/CityDetailViewController.swift
@@ -14,7 +14,7 @@ final class CityDetailViewController: UIViewController {
     @IBOutlet private weak var cityImage: UIImageView!
     @IBOutlet private weak var descriptionLabel: UILabel!
     
-    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var chartView: LineChartView!
     
     var months: [String]!
     
@@ -29,12 +29,71 @@ final class CityDetailViewController: UIViewController {
         
           setupData()
         
-        // barChartView.noDataTextDescription = "Just because" type BarChartView doest have noDataTextDescription
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let unitsSold = [50.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        // Do any additional setup after loading the view.
+   //     self.title = "Time Line Chart"
         
-        //setChart(months, values: unitsSold) missing argument label dataPoints: in call
-        setChart(dataPoints: months, values: unitsSold)
+        //chartView.delegate = self
+        
+        self.title = "Line Chart 1"
+        
+        chartView.chartDescription?.enabled = false
+        chartView.dragEnabled = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = true
+        
+        // x-axis limit line
+        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
+        llXAxis.lineWidth = 4
+        llXAxis.lineDashLengths = [10, 10, 0]
+        llXAxis.labelPosition = .rightBottom
+        llXAxis.valueFont = .systemFont(ofSize: 10)
+        
+        chartView.xAxis.gridLineDashLengths = [10, 10]
+        chartView.xAxis.gridLineDashPhase = 0
+        
+        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
+        ll1.lineWidth = 4
+        ll1.lineDashLengths = [5, 5]
+        ll1.labelPosition = .rightTop
+        ll1.valueFont = .systemFont(ofSize: 10)
+        
+        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
+        ll2.lineWidth = 4
+        ll2.lineDashLengths = [5,5]
+        ll2.labelPosition = .rightBottom
+        ll2.valueFont = .systemFont(ofSize: 10)
+        ll2.lineColor = ChartColorTemplates.colorFromString("#0000ff")
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.removeAllLimitLines()
+        leftAxis.addLimitLine(ll1)
+        leftAxis.addLimitLine(ll2)
+        leftAxis.axisMaximum = 200
+        leftAxis.axisMinimum = -50
+        leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.drawLimitLinesBehindDataEnabled = true
+        
+        chartView.rightAxis.enabled = false
+        
+        //[_chartView.viewPortHandler setMaximumScaleY: 2.f];
+        //[_chartView.viewPortHandler setMaximumScaleX: 2.f];
+        
+       /* let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
+                                   font: .systemFont(ofSize: 12),
+                                   textColor: .white,
+                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
+        marker.chartView = chartView
+        marker.minimumSize = CGSize(width: 80, height: 40)
+        chartView.marker = marker*/
+        
+        chartView.legend.form = .line
+        
+       /* sliderX.value = 45
+        sliderY.value = 100
+        slidersValueChanged(nil)*/
+        
+        chartView.animate(xAxisDuration: 2.5)
+        self.setDataCount(5, range: 5)
     }
     
       private func setupData() {
@@ -49,26 +108,51 @@ final class CityDetailViewController: UIViewController {
      cityImage.kf.setImage(with: city.imageUrl, placeholder: UIImage(named: Constants.cityImagePlaceholder))
      }
     
-    func setChart(dataPoints: [String], values: [Double]) {
-        barChartView.noDataText = "You need to provide data for the chart."
-        var dataEntries: [BarChartDataEntry] = []
+    func setDataCount(_ count: Int, range: UInt32) {
+       /* let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(range) + 3)
+            return ChartDataEntry(x: Double(i), y: val, icon: #imageLiteral(resourceName: "icon"))
+        }*/
         
-        for i in 0..<dataPoints.count {
-            // let dataEntry = BarChartDataEntry(value: values[i], xIndex: i) // argument labeld dont match
-            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i], data: dataPoints[i] as AnyObject)
-            dataEntries.append(dataEntry)
+        let now = Date().timeIntervalSince1970
+        let hourSeconds: TimeInterval = 3600
+        
+        let from = now - (Double(count) / 2) * hourSeconds
+        let to = now + (Double(count) / 2) * hourSeconds
+        
+        let values = stride(from: from, to: to, by: hourSeconds).map { (x) -> ChartDataEntry in
+            let y = arc4random_uniform(range) + 50
+            return ChartDataEntry(x: x, y: Double(y))
         }
         
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
         
-        // let chartData = BarChartData(xVals: months, dataSet: chartDataSet) cannot invoke initializer BarCharData with arg list of (xVals: [String]!, dataSet: BarCharDataSet
-        let chartData    = BarChartData(dataSets: [chartDataSet])
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        set1.drawIconsEnabled = false
         
-        barChartView.data = chartData
-        //barChartView.xAxis.ent
-        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
-        barChartView.xAxis.labelCount = months.count
-        barChartView.xAxis.labelPosition = .bottom
+        set1.lineDashLengths = [5, 2.5]
+        set1.highlightLineDashLengths = [5, 2.5]
+        set1.setColor(.black)
+        set1.setCircleColor(.black)
+        set1.lineWidth = 1
+        set1.circleRadius = 3
+        set1.drawCircleHoleEnabled = false
+        set1.valueFont = .systemFont(ofSize: 9)
+        set1.formLineDashLengths = [5, 2.5]
+        set1.formLineWidth = 1
+        set1.formSize = 15
         
+        let gradientColors = [ChartColorTemplates.colorFromString("#0000ff00").cgColor,
+                              ChartColorTemplates.colorFromString("#63e552").cgColor]
+      //  let gradientColors = [ChartColorTemplates.colorFromString("#00ff0000").cgColor,
+      //                        ChartColorTemplates.colorFromString("#ffff0000").cgColor]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+        
+        set1.fillAlpha = 1
+        set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
+        set1.drawFilledEnabled = true
+        
+        let data = LineChartData(dataSet: set1)
+        
+        chartView.data = data
     }
 }
