bbeee5e61bf1a69466cdc61217dd05dad15e573d
aaaus <aaausaaa@gmail.com>
2019-02-27 18:54:21 +0200

    3

--- CityMap/View/Cities/CityViewCell.swift
+++ CityMap/View/Cities/CityViewCell.swift
@@ -28,7 +28,7 @@ class CityViewCell: UICollectionViewCell {
 
     private func update() {
        // cityLabel.text = city?.name
-        cityLabel.text = "Shostka25"
+        cityLabel.text = "Shostka25_1"
 
         // Set image using url to the image view using kingfisher extension.
         //cityImage.kf.setImage(with: city?.imageUrl, placeholder: UIImage(named: Constants.cityImagePlaceholder))
