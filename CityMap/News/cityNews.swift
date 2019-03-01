961ac1fe5df40ffa871923ed0b1cec2d60d28b85
aaaus <aaausaaa@gmail.com>
2019-03-01 17:25:16 +0200

    12.1 News

--- CityMap/News/cityNews.swift
+++ CityMap/News/cityNews.swift
@@ -17,6 +17,7 @@ class cityNews: UIViewController {
      /*   func loadHtmlCode() {
             let htmlCode = "<p>У Шостці була створена робоча група по виявленню джерела хімічного запаху, на який час від часу скаржаться містяни. За участі членів робочої групи, та представників  потенційно-небезпечних підприємств, що розташовані у Шостці відбулась робоча нарада. Під час неї присутні намагались з&#8217;ясувати, яке підприємство причетне до розповсюдження їдкого смороду. Крім того, було запропоновано створити лабораторію, завдяки якій можна буде виявити чи є викид певних хімічних елементів у повітря.</p>\n<p><iframe  id=\"_ytid_93379\" width=\"560\" height=\"320\" src=\"https://www.youtube.com/embed/vtL3vSC0YnI?enablejsapi=1&#038;autoplay=0&#038;cc_load_policy=0&#038;iv_load_policy=1&#038;loop=0&#038;modestbranding=0&#038;rel=1&#038;showinfo=1&#038;fs=1&#038;playsinline=0&#038;autohide=2&#038;theme=dark&#038;color=red&#038;controls=2&#038;\" class=\"__youtube_prefs__\" title=\"YouTube player\"  allowfullscreen data-no-lazy=\"1\" data-skipgform_ajax_framebjll=\"\"></iframe></p>\n"
             webView.loadHTMLString(htmlCode, baseURL: nil)
+            https://www.shostka.info/wp-json/wp/v2/posts/
         }*/
         
         
