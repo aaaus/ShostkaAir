47c383143fd4a5696babc08e4453d07c995ef556
aaaus <aaausaaa@gmail.com>
2019-03-02 15:39:52 +0200

    16 ReadyForSites

---
+++ CityMap/News/PostViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PostViewController.swift
+//  CityMap
+//
+//  Created by Aleksandr Andrusenko on 02.03.2019.
+//  Copyright © 2019 it-shark. All rights reserved.
+//
+
+import UIKit
+import WordPress
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CityMap/News/String+Convenience.swift
@@ -0,0 +1,40 @@
+//
+//  String+Convenience.swift
+//  CityMap
+//
+//  Created by Aleksandr Andrusenko on 02.03.2019.
+//  Copyright © 2019 it-shark. All rights reserved.
+//
+
+import UIKit
+
+
+// original idea from https://stackoverflow.com/questions/25607247/how-do-i-decode-html-entities-in-swift
+
+extension String {
+    init(htmlEncodedString: String) {
+        self.init()
+        if let attributedString = htmlEncodedString.htmlToAttributedString() {
+            self = attributedString.string
+        }
+        else {
+            self = htmlEncodedString
+        }
+    }
+    
+    func htmlToAttributedString() -> NSAttributedString? {
+        if let data = self.data(using:.utf8) {
+            // pm171004 converted to Swift 4
+            let options:[NSAttributedString.DocumentReadingOptionKey : Any] =
+                [NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.documentType.rawValue): NSAttributedString.DocumentType.html,
+                 NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.characterEncoding.rawValue): String.Encoding.utf8.rawValue]
+            
+            if let attributedString = try? NSAttributedString(data:data, options:options, documentAttributes:nil) {
+                return attributedString
+            }
+        }
+        return nil
+    }
+    
+}
+
