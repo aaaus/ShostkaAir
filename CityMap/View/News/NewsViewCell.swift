f3478e04bce158f5456358e28fb92037e9470132
aaaus <aaausaaa@gmail.com>
2019-03-04 14:53:28 +0200

    20 sensorsCountWork

--- CityMap/View/News/NewsViewCell.swift
+++ CityMap/View/News/NewsViewCell.swift
@@ -1,45 +1,46 @@
+//
+//  SensorsViewCell.swift
+//  CityMap
+//
+//  Created by Aleksandr Andrusenko on 25.02.2019.
+//  Copyright © 2019 it-shark. All rights reserved.
+//
+
 import UIKit
 import Kingfisher
 
 private enum Constants {
     // Placeholder image for the cities that don't have photo (has the same name as appropriate image in assets folder).
     static let cityImagePlaceholder = "CityPlaceholder"
-    static let cityLogo = "Logo"
 }
 
-/**
-    Custom collection view cell to show brief information about the city it contains.
- */
-class CityViewCell: UICollectionViewCell {
-
+class NewsViewCell: UICollectionViewCell {
+    
     @IBOutlet private weak var cityImage: UIImageView!
     @IBOutlet private weak var cityLabel: UILabel!
-
+    
     /**
-        Get currently represented city from cell or set the new city to update cell information.
-        If you set nil value, city information will be cleared from cell.
-    */
+     Get currently represented city from cell or set the new city to update cell information.
+     If you set nil value, city information will be cleared from cell.
+     */
     var city: City? {
         didSet {
             update()
         }
-     
     }
-
+    
     private func update() {
         cityLabel.text = city?.name
-       // var testLbl = city?.id
-       // cityLabel.text = "Shostka"
-
+        
         // Set image using url to the image view using kingfisher extension.
         cityImage.kf.setImage(with: city?.imageUrl, placeholder: UIImage(named: Constants.cityImagePlaceholder))
-        //cityImage.image = UIImage(named: "Logo")
     }
-
+    
     override func prepareForReuse() {
         // Before reuse the cell - clear it ...
         cityLabel.text = nil
         // ... and cancel image loading using Kingfisher extension.
         cityImage.kf.cancelDownloadTask()
     }
+    
 }
---
+++ CityMap/View/News/NewsViewController.swift
@@ -0,0 +1,128 @@
+import UIKit
+
+private enum Segues: String {
+    // Segue name that navigates to city detail page (has the same value as the segue in storyboard).
+    case showCityDetail = "ShowCityDetail"
+    // Segue name that navigates to the map page (has the same value as the segue in storyboard).
+    case showMap = "ShowMap"
+}
+
+private enum Constants {
+    // City cell reuse identifier (has the same value as city cell in storyboard) to use table view cell recycling mechanism.
+    static let cityCellIdentifier = "CityCellIdentifier"
+}
+
+/**
+ All cities controller to show all available cities as a collection.
+ */
+class NewsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    private let cityService = CityService.shared
+    private var cities: [City] = [] {
+        didSet {
+            // Marshal cities to the main thread using GCD.
+            DispatchQueue.main.async { [weak self] in
+                // Update collection as data source has changed.
+                self?.collectionView.reloadData()
+                self?.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    var city: City? {
+        didSet {
+            //  update()
+        }
+    }
+    var sensorStationNumber: Int = 0
+    // var laboratoryCount: Int = 0
+    
+    var sensorsArrayCountBeforeCurentSensors: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("123")
+        guard let city = city else {
+            return
+        }
+        print("4567")
+       // print("city.id: \(city.id)")
+       // sensorStationNumber = city.id
+        setupData()
+    }
+    
+    private func setupData() {
+        activityIndicator.startAnimating()
+        
+        cityService.cities { [weak self] cities in
+            self?.cities = cities
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let segueIdentifier = segue.identifier else {
+            return
+        }
+        
+        switch segueIdentifier {
+        case Segues.showCityDetail.rawValue:
+            guard let cityCell = sender as? SensorsViewCell,
+                let detailsController = segue.destination as? CityDetailViewController else {
+                    return
+            }
+            
+            detailsController.city = cityCell.city
+            detailsController.sensorStationNumber = sensorStationNumber
+        case Segues.showMap.rawValue:
+            guard let mapController = segue.destination as? MapViewController else {
+                return
+            }
+            
+            mapController.cities = cities
+        default:
+            break
+        }
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var citiesCount = 0
+        sensorsArrayCountBeforeCurentSensors = 0
+        
+        for city in cities {
+            
+            if (city.id >= (sensorStationNumber + 1) * 100) && (city.id < (sensorStationNumber + 2) * 100){  // Сколько датчиков у лаборатории
+                citiesCount += 1
+                print(city.id)
+            }
+            if city.id < (sensorStationNumber + 1) * 100{  // Сколько объектов в массиве до текущей лаборатории
+                sensorsArrayCountBeforeCurentSensors += 1
+            }
+        }
+        
+        if cities.count == 0 {  // проверем если 0, бывает первый раз, возвращаем 0
+            return 0
+        }
+        
+        return 2    // Нужное количество лабараторий
+        
+        
+        // return cities.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cityCell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cityCellIdentifier, for: indexPath) as? SensorsViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let capitalToDisplay = cities[indexPath.row + 0]
+        cityCell.city = capitalToDisplay
+        print("cityCell\(cityCell)")
+        return cityCell
+    }
+}
+
